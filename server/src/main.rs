5d42749250747ada2f70107a8e36d6dd61be1074
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-11-12 14:37:30 +0530

    server: request-response formats

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,36 @@
+use std::collections::HashMap;
+
 use anyhow::Result;
 use axum::routing::post;
 use axum::Router;
+use serde::Deserialize;
+use serde::Serialize;
+
+#[derive(Deserialize, Serialize)]
+struct Request {
+    url: String,
+    method: String,
+    headers: HashMap<String, String>,
+    excluded_headers: HashMap<String, String>,
+    body: String,
+    excluded_body: String,
+    response_headers: Vec<String>,
+}
+
+#[derive(Deserialize, Serialize)]
+struct Response {
+    handler: u8,
+    status: u16,
+    headers: HashMap<String, String>,
+    body: String,
+    signature: String,
+}
 
 async fn teefetch() {}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let app = Router::new().route("/", post(teefetch));
+    let app = Router::new().route("/json", post(teefetch));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
 
     axum::serve(listener, app).await?;
