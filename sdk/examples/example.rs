45376bc0e4f5a4c32cd076c436a704b9458575b8
Vikrant Garg <vikrant1garg@gmail.com>
2024-11-12 19:24:05 +0530

    sdk: integrate with TEE server

--- sdk/examples/example.rs
+++ sdk/examples/example.rs
@@ -14,14 +14,16 @@ async fn main() {
     let args = Cli::parse();
     // TODO: Change to your Oyster device IP address
     let client = OysterHttpsClient::new(&args.ip);
+    let mut headers = HashMap::new();
+    headers.insert("Host".to_string(), "example.com".to_string());
     let request = Request {
-        url: "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd".to_string(),
+        url: "https://example.com".to_string(),
         method: "GET".to_string(),
-        headers: HashMap::new(),
+        headers,
         excluded_headers: HashMap::new(),
         body: "".to_string(),
         excluded_body: "".to_string(),
-        response_headers: vec!["Content-Type".to_string()],
+        response_headers: vec![],
     };
     let response = client.oyster_fetch(&request).await.unwrap();
     println!("{:?}", serde_json::to_string(&response).unwrap());
--- sdk/src/client.rs
+++ sdk/src/client.rs
@@ -1,9 +1,38 @@
+use alloy::hex;
+use alloy::sol;
+use alloy::sol_types::eip712_domain;
+use alloy::sol_types::SolStruct;
 use anyhow::Result;
 use k256::ecdsa::{RecoveryId, Signature, VerifyingKey};
 use oyster::{decode_attestation, get_attestation_doc, verify_with_timestamp};
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+
+sol! {
+    struct RequestData {
+        string url;
+        string method;
+        string[] headerKeys;
+        string[] headerValues;
+        string body;
+        string[] responseHeaders;
+    }
+
+    struct ResponseData {
+        uint8 handler;
+        uint16 status;
+        string[] headerKeys;
+        string[] headerValues;
+        string body;
+        uint64 timestamp;
+    }
+
+    struct RequestResponseData {
+        RequestData requestData;
+        ResponseData responseData;
+    }
+}
 
 pub struct OysterHttpsClient {
     client: Client,
@@ -16,20 +45,32 @@ impl OysterHttpsClient {
     }
 
     pub async fn oyster_fetch(&self, request: &Request) -> Result<Response> {
-        let oyster_url = format!("https://{}:8080/", self.ip);
+        let oyster_url = format!("http://{}:3000/json", self.ip);
         let data = serde_json::to_string(&request).unwrap();
-        let response = self.client.post(oyster_url).body(data).send().await?.text().await?;
-        let response: Response = serde_json::from_str(&response)?;
-        Ok(response)
+        let response = self
+            .client
+            .post(oyster_url)
+            .header("Content-Type", "application/json")
+            .body(data)
+            .send()
+            .await?;
+
+        // raise error if response is not ok
+        if !response.status().is_success() {
+            return Err(anyhow::anyhow!("Request failed with status: {}", response.status()));
+        }
+
+        let response_data: Response = response.json().await?;
+        Ok(response_data)
     }
     pub async fn verify_signature(&self, request: &Request, response: &Response) -> Result<()> {
-        // TODO: Implement signature verification
         // Verify attestation
-        let attestation_doc = get_attestation_doc(format!("https://{}:1300/attestation", self.ip).parse()?).await?;
+        let attestation_doc = get_attestation_doc(format!("http://{}:1300/attestation", self.ip).parse()?).await?;
         let parsed = decode_attestation(attestation_doc.clone())?;
-        let public_key = verify_with_timestamp(attestation_doc, parsed.pcrs, parsed.timestamp)?;
+        let expected_verifying_key = hex::encode(verify_with_timestamp(attestation_doc, parsed.pcrs, parsed.timestamp)?);
         // Create digest
-        let digest = Self::create_digest(request);
+        let digest = Self::create_digest(request, response);
+        println!("{:?}", hex::encode(digest.clone()));
         // Verify signature
         let signature_with_recovery = hex::decode(&response.signature)?;
         let signature = Signature::from_slice(&signature_with_recovery[..64])?;
@@ -41,12 +82,39 @@ impl OysterHttpsClient {
             &signature,
             rec_id
         )?;
-        assert_eq!(public_key, recovered_key.to_sec1_bytes().to_vec());
+        let recoverd_key_hex = hex::encode(recovered_key.to_encoded_point(false).as_bytes()).split_off(2);
+        println!("recovered_key: {:?}", recoverd_key_hex);
+        assert_eq!(expected_verifying_key, recoverd_key_hex);
         Ok(())
     }
-    fn create_digest(request: &Request) -> Vec<u8> {
+    fn create_digest(request: &Request, response: &Response) -> Vec<u8> {
         // TODO: Implement digest creation
-        "".as_bytes().to_vec()
+        let domain = eip712_domain! {
+            name: "Teefetch",
+            version: "1",
+        };
+
+        let signing_struct = RequestResponseData {
+            requestData: RequestData {
+                url: request.url.clone(),
+                method: request.method.clone(),
+                headerKeys: request.headers.keys().map(|k| k.to_owned()).collect(),
+                headerValues: request.headers.values().map(|k| k.to_owned()).collect(),
+                body: request.body.clone(),
+                responseHeaders: request.response_headers.clone(),
+            },
+            responseData: ResponseData {
+                handler: 1,
+                status: response.status,
+                headerKeys: response.headers.keys().map(|k| k.to_owned()).collect(),
+                headerValues: response.headers.values().map(|k| k.to_owned()).collect(),
+                body: response.body.clone(),
+                timestamp: response.timestamp,
+            },
+        };
+        let signing_hash = signing_struct.eip712_signing_hash(&domain);
+
+        signing_hash.to_vec()
     }
 }
 
@@ -67,5 +135,6 @@ pub struct Response {
     pub status: u16,
     pub headers: HashMap<String, String>,
     pub body: String,
+    pub timestamp: u64,
     pub signature: String,
 }
