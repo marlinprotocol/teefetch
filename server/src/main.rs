e973b1d4ee66bfdd6028a2ce70928ab46f6ef91a
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-11-12 11:46:06 +0530

    server: basic axum scaffolding

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use axum::routing::post;
+use axum::Router;
+
+async fn teefetch() {}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app = Router::new().route("/", post(teefetch));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
