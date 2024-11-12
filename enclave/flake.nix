97c969039773b4a7528a2eaead7a31c828ac62de
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-11-12 19:51:00 +0530

    enclave: add a simple echo flake that expects the server derivation as input

---
+++ enclave/flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    server.url = "";
+  };
+  outputs = { self, nixpkgs, server }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages."${system}";
+    in
+    {
+      packages.${system}.default = server.packages.${system}.default;
+    };
+}
