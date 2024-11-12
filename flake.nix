b52d79193666dd54fa9039e361a709ac5f745c24
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-11-12 19:51:32 +0530

    _: add enclave to the root flake and pass in the server

--- flake.nix
+++ flake.nix
@@ -5,13 +5,21 @@
       url = "path:./server";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    enclave = {
+      url = "path:./enclave";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.server.follows = "server";
+    };
   };
-  outputs = { self, nixpkgs, server }:
+  outputs = { self, nixpkgs, server, enclave }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages."${system}";
     in
     {
-      packages.${system}.server = server.packages.${system}.default;
+      packages.${system} = {
+        server = server.packages.${system}.default;
+        enclave = enclave.packages.${system}.default;
+      };
     };
 }
